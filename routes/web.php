71a06fe866df00125a246a8c7313c0263104b48a
sternenfall <sternenfall.1995@gmail.com>
2024-05-06 17:48:48 +0900

    ブログ投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ use App\Http\Controllers\PostController;      //追加
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
 Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
