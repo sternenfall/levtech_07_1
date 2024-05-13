4032b447756ce38d6fe0e5434936f01e48d91b9a
sternenfall <sternenfall.1995@gmail.com>
2024-05-13 17:25:42 +0900

    マイグレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\PostRequest; // useする
+use App\Http\Requests\PostRequest; 
 
 class PostController extends Controller
 {
